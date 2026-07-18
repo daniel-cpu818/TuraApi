bccf81b58db2b175ad47bc0012811f43a18fe1cd
Daniel Jassan <115967620+daniel-cpu818@users.noreply.github.com>
2026-07-18 00:56:44 -0500

    Update EF migrations

--- src/BookingTura.Infrastructure/Data/Migrations/20260718055357_UpdateRailway.Designer.cs
+++ src/BookingTura.Infrastructure/Data/Migrations/20260718055357_UpdateRailway.Designer.cs
@@ -4,6 +4,7 @@ using BookingTura.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookingTura.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(BookingTuraDbContext))]
-    partial class BookingTuraDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260718055357_UpdateRailway")]
+    partial class UpdateRailway
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
